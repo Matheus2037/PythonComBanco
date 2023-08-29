dc35752f670f2d1e8de901e05b04640bc398d567
Matheus Silva Gastaldi <matheusdasilvagastaldi@gmail.com>
2023-08-28 21:47:42 -0300

    Enviando comandos para o banco
    Comandos enviados ao banco por meio de escolha do usuário, 4 opções padrões, (INSERIR, EDITAR, EXCLUIR, PESQUISAR), com valores prontos e fixos

--- PRATICA02/DBcommand.py
+++ PRATICA02/DBcommand.py
@@ -1,29 +1,44 @@
 import ConBanco as DBCON
 import sqlite3
 from sqlite3 import Error
-
 DBCON.vcon
 
-id = 1
-nome = "Matheus"
-saldo = 100000
-dados=(id, nome, saldo)
+option = int(input("Digite qual ação deseja realizar: "
+                   "\n1-Inserir valores"
+                   "\n2-Alterar valores"
+                   "\n3-Excluir valores"
+                   "\n4-Filtrar valores\n"))
 
-vsql="""CREATE TABLE TB_PESSOAS(
-        N_IDPessoa INTEGER PRIMARY KEY AUTOINCREMENT,
-        T_NOMEPessoa VARCHAR (30),
-        N_SALDO INTEGER
-        );"""
-vatt= """UPDATE TB_PESSOAS SET N_IDPessoa = ?, T_NOMEPessoa = ?, N_SALDO = ? """
+if option == 1:
+    comando_temp = """INSERT INTO TB_PESSOAS
+                        (T_NOMEPessoa,N_SALDO)
+                        VALUES('Robson','90000')"""
+elif option == 2:
+    comando_temp = """UPDATE TB_PESSOAS 
+                        SET T_NOMEPessoa = 'João', N_SALDO = 47500 WHERE N_IDPessoa = 2"""
+elif option == 3:
+    comando_temp = "DELETE FROM TB_PESSOAS WHERE N_IDPessoa = 1"
+elif option == 4:
+    comando_temp = "SELECT T_NOMEPessoa,N_SALDO FROM TB_PESSOAS WHERE N_IDPessoa = 2"
 
-def ADICIONARVALORES(conexao,sql,data):
+vsql = comando_temp
+
+
+def Commands(conexao,sql):
     try:
         c=conexao.cursor()
-        c.execute(sql, data)
-        DBCON.vcon.commit()
-        print("DADOS CADASTRADOS!")
+        c.execute(sql)
+        conexao.commit()
+        if option == 4:
+            resultado = c.fetchone()
+            nome, saldo = resultado
+            if resultado != "":
+                print(f"O resultado da busca foi o usuário: {nome}, com saldo de {saldo}")
+            else:
+                print("Nenhum usuário encontrado!")
+        print("Comando Executado com sucesso!")
     except Error as ex:
         print(ex)
 
-teste = ADICIONARVALORES(DBCON.vcon, vatt, dados)
-    
+
+Commands(DBCON.vcon,vsql)
